621177fb79b39bc401e0d6e64477a2de27d7ef68
CasparEkroth <caspar.ekroth@gmail.com>
2025-06-08 13:01:03 +0200

    started working on the severCommand

--- src/main/java/com/myname/cemount/commands/ServerCommand.java
+++ src/main/java/com/myname/cemount/commands/ServerCommand.java
@@ -1,6 +1,11 @@
 package com.myname.cemount.commands;
 
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * ServeCommand listens on a TCP port and accepts a single `cem push` from a client.
  * Usage: cem serve <port> <path-to-bare-repo>
@@ -13,9 +18,31 @@ package com.myname.cemount.commands;
 public class ServerCommand {
     // for setting upp a server
     // cem serve --port <n>
+    private static final String CEM_DB_DIR = "CEMountDB";
     private static final String CEM_DIR   = ".cemount";
     private static final String REFS_DIR  = "refs";
     private static final String HEADS_DIR = "heads";
     private static final String OBJECTS   = "objects";
     private static final String HEAD_FILE = "HEAD";
+
+    public static void execute(String[] args){
+        if(args.length != 2){
+            System.err.println("Usage: cem server <port> <file path for CEMount>");
+            return;
+        }
+        String port = args[0];
+        String path = args[1];
+        try{
+            Path dbDir = createDataBaseDir(path);
+        }catch (IOException e){
+            System.out.println("d" + e.getMessage());
+        }
+
+    }
+
+    private static Path createDataBaseDir(String path) throws IOException {
+        Path serverPath = Paths.get(path).toAbsolutePath().normalize().resolve(ServerCommand.CEM_DB_DIR);
+        Files.createDirectories(serverPath);
+        return serverPath;
+    }
 }
