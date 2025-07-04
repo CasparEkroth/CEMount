60b7712a92b302f2aa231099f07d97757c179b5b
CasparEkroth <caspar.ekroth@gmail.com>
2025-07-04 21:06:27 +0200

    working on the fetch command

--- src/main/java/com/myname/cemount/commands/FetchCommand.java
+++ src/main/java/com/myname/cemount/commands/FetchCommand.java
@@ -2,7 +2,9 @@ package com.myname.cemount.commands;
 
 import com.myname.cemount.server.ObjectUtils;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
@@ -37,6 +39,36 @@ public class FetchCommand {
         }
         String remoteUrl = remote.get(remoteName);
         String branch = ObjectUtils.getBranch(cemDir);
-        String shaRef = ObjectUtils.getRef(cemDir,branch);
+        String shaRef = ObjectUtils.getRef(cemDir,branch).trim();
+
+        String without = remoteUrl.substring("tcp://".length());
+        int idx1   = without.indexOf(':');
+        int slash  = without.lastIndexOf('/');
+        String serverPort = without.substring(idx1 + 1,
+                slash < idx1 ? without.length() : slash);
+        String serverIP   = without.substring(0, idx1);
+
+        try(Socket socket = new Socket(serverIP, Integer.parseInt(serverPort));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8))
+        ){
+            out.write("FETCH " + repoName + " " + branch + "\n");
+            out.flush();
+
+            String respons = in.readLine().trim();
+            if(respons.equals(shaRef)){
+                System.out.println("Already up to date.");
+                out.write("OK \n");
+                out.flush();
+                return;
+            }else {
+                out.write(shaRef + "\n");
+                out.flush();
+
+                in.readLine();
+            }
+            // add logig for the fetch (on server also)
+
+        }
     }
 }
